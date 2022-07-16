9d238daae2527fc8d201988a73c570c83c62f56d
sajib-hosen <sajib.201h@gmail.com>
2022-07-16 14:01:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+{/* .textStoke {
+    background-color: #61dafb; 
+    color: #ffffff;
+    -webkit-text-fill-color: transparent; 
+    -webkit-text-stroke: 0.1em transparent;
+    text-transform: uppercase;
+    letter-spacing: 0.2em;
+    background: linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet);
+    -webkit-background-clip: text;
+  } */}
+
+      <div className='border border-red-200 h-36 relative'>
+        <div className='flex justify-start '>
+           <span className="mx-20 text-left font-poppins textStoke">Text Stoke</span>
+        </div>
+
+          <div className='absolute border top-10 left-10' >
+            <p className='text-2xl '>this is small text</p>
+          </div>
+
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+// https://design-creation.webflow.io/
